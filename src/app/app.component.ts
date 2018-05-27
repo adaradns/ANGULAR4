ce9f52fd7b7636e4d123e87b3645a738aabf88b7
adaradenis@gmail.com <adaradenis@gmail.com>
2018-05-27 20:34:02 -0300

    Directiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,31 @@ export class AppComponent {
   title = 'Angular 4';
   description = 'Esta es la descripcion de mi pagina con angular 4';
   name = '';
+  notas = [
+	  {
+	  	id : 1,
+	  	titulo : 'Nota 1',
+	  	descripcion: 'Esta es mi nota numero 1'	
+	  },
+	  {
+	  	id : 2,
+	  	titulo : 'Nota 2',
+	  	descripcion: 'Esta es mi nota numero 2'	
+	  },
+	  {
+	  	id : 3,
+	  	titulo : 'Nota 3',
+	  	descripcion: 'Esta es mi nota numero 3'	
+	  },
+	  {
+	  	id : 4,
+	  	titulo : 'Nota 4',
+	  	descripcion: 'Esta es mi nota numero 4'	
+	  },
+	  {
+	  	id : 5,
+	  	titulo : 'Nota 5',
+	  	descripcion: 'Esta es mi nota numero 5'	
+	  }
+  ];
 }
